cc17e16a840bb94913a64d063da1fb9e41ac0c00
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-12-10 09:54:04 -0300

    feat: Botão Sair

--- app/src/main/java/com/example/oscar_app/TelaInicial.kt
+++ app/src/main/java/com/example/oscar_app/TelaInicial.kt
@@ -14,10 +14,28 @@ class TelaInicial : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_inicial)
         val btnsair = findViewById<Button>(R.id.sairButton)
-
+        val btnfilm = findViewById<Button>(R.id.votarFilmeButton)
+        val btndiretor = findViewById<Button>(R.id.votarDiretorButton)
+        val btnconfirmar = findViewById<Button>(R.id.confirmarButton)
         btnsair.setOnClickListener {
             finishAffinity()
         }
+
+        btnfilm.setOnClickListener {
+            val telaFilme = Intent(this, VotarFilme::class.java)
+            startActivity(telaFilme);
+        }
+
+        btndiretor.setOnClickListener {
+            val telaDiretor = Intent(this, VotarDiretor::class.java)
+            startActivity(telaDiretor);
+        }
+
+        btnconfirmar.setOnClickListener {
+            val telaConfirmar = Intent(this, ConfirmarVoto::class.java)
+            startActivity(telaConfirmar);
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
