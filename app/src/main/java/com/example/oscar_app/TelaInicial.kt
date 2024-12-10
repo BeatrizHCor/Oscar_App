db9e7e830398b7170c077100d3aa1aa5ee493343
BeatrizHCor <92833059+BeatrizHCor@users.noreply.github.com>
2024-12-10 09:45:46 -0300

    feat: Botão Sair

--- app/src/main/java/com/example/oscar_app/TelaInicial.kt
+++ app/src/main/java/com/example/oscar_app/TelaInicial.kt
@@ -1,6 +1,8 @@
 package com.example.oscar_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,11 @@ class TelaInicial : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_inicial)
+        val btnsair = findViewById<Button>(R.id.sairButton)
+
+        btnsair.setOnClickListener {
+            finishAffinity()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
